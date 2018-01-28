31627fbcd93887d793b8eb2e18109d6a58a2902e
Diego Vargas <diegofvargas@gmail.com>
2018-01-28 15:39:32 -0500

    Added object to handle strings

--- app.js
+++ app.js
@@ -3,18 +3,29 @@ var budgetController = (function() {
 })();
 
 var UIController = (function() {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
+    
     return {
         getInput: function() {
             return {
-                type: document.querySelector('.add__type').value, //will be income or expense selected
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value, //will be income or expense selected
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             };
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
         }
     };
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
+    var DOM = UICtrl.getDOMstrings();
     var ctrlAddItem = function() {
         //Get input data
         var input = UICtrl.getInput();
@@ -25,7 +36,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         //display budget
     }
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         if(event.keyCode === 13 ||event.which === 13) {
