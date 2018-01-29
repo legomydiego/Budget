421785d59dcecd99cefc51039bb53990c147c092
Diego Vargas <diegofvargas@gmail.com>
2018-01-28 21:52:42 -0500

    Added DOM manipulation

--- app.js
+++ app.js
@@ -19,6 +19,28 @@ var budgetController = (function() {
             exp: 0,
             inc: 0
         }
+    };
+    return {
+        addItem: function(type,des,val) {
+            var newItem, ID;
+            //create new ID
+            if(data.allItems[type].length >0){
+                ID = data.allItems[type][data.allItems[type].length-1].id+1;
+            }   else{
+                ID = 0;
+            }
+            
+            //create new item based on inc or exp
+            if(type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            //push into data structure
+            data.allItems[type].push(newItem);
+            //return new element
+            return newItem;
+        }
     }
 })();
 
@@ -27,7 +49,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputButton: '.add__btn'
+        inputButton: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
     
     return {
@@ -38,6 +62,26 @@ var UIController = (function() {
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         },
+        
+        addListItem: function(obj,type) {
+            //create html string with placerholder text
+            var html, newhtml, element;
+            if(type==='inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if(type==='exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+            //replace placeholder text with data from object
+            newhtml = html.replace('%id%', obj.id);
+            newhtml = newhtml.replace('%description%', obj.description);
+            newhtml = newhtml.replace('%value%', obj.value);
+            //insert html into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend',newhtml);
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -58,7 +102,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         //Get input data
         var input = UICtrl.getInput();
         //add the item to budget controller
+        var newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         //add to the UI
+        UICtrl.addListItem(newItem,input.type);
         //Calculate Budget
         //display budget
     };
