3727b68ec7fb255b11dd37218ced7a158fd35699
Diego Vargas <diegofvargas@gmail.com>
2018-01-30 00:43:47 -0500

    Update UI after deleting list items

--- app.js
+++ app.js
@@ -51,6 +51,17 @@ var budgetController = (function() {
             //return new element
             return newItem;
         },
+        deleteItem: function(type, id) {
+            var ids, index;
+            ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+            index = ids.indexOf(id);
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
         calculateBudget: function() {
             //calculate total inc and exp
             calculateTotal('exp');
@@ -86,7 +97,8 @@ var UIController = (function() {
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
     }
 
     return {
@@ -103,10 +115,10 @@ var UIController = (function() {
             var html, newhtml, element;
             if (type === 'inc') {
                 element = DOMstrings.incomeContainer;
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
                 element = DOMstrings.expensesContainer;
-                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
 
             //replace placeholder text with data from object
@@ -115,6 +127,11 @@ var UIController = (function() {
             newhtml = newhtml.replace('%value%', obj.value);
             //insert html into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newhtml);
+        },
+
+        deleteListItem: function(selectorID) {
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
         },
 
         clearFields: function() {
@@ -154,6 +171,7 @@ var controller = (function(budgetCtrl, UICtrl) {
                 ctrlAddItem();
             }
         });
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     }
     var updateBudget = function() {
         //Calculate Budget
@@ -177,6 +195,21 @@ var controller = (function(budgetCtrl, UICtrl) {
             updateBudget();
         }
     };
+    var ctrlDeleteItem = function(event) {
+        var itemID, splitID, type, ID;
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        if (itemID) {
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = parseInt(splitID[1]);
+            //delete item from data structure
+            budgetCtrl.deleteItem(type, ID);
+            //delete from user interface
+            UICtrl.deleteListItem(itemID);
+            //show the new values in UI
+            updateBudget();
+        }
+    }
     return {
         init: function() {
             console.log('Application has started');
